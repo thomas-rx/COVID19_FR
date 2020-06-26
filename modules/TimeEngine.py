420ae1b8f4704b8b9c20ff41ba6b22af63c8bf3e
Rached Mejri <r.mejri74100@gmail.com>
2020-06-27 01:36:10 +0200

    improving et reducing of get_days function

--- modules/TimeEngine.py
+++ modules/TimeEngine.py
@@ -13,20 +13,16 @@ def get_days():
     containment_date = datetime(2020, 3, 17)  # Confinement
     today_date = datetime.today()
 
-    number_of_days = str(today_date - containment_date)
-    number_of_days = str([int(s) for s in number_of_days.split() if s.isdigit()])
-    number_of_days = number_of_days.replace('[', '')
-    number_of_days = number_of_days.replace(']', '')
-    number_of_days = int(number_of_days) + 1  # Le confinement a débuté à midi.
-
-    return str(number_of_days)
+    # Le confinement a débuté à midi.
+    return str((today_date - containment_date).days - 1)
 
 
 def check_time():
     if get_config_boolean('System', 'checkTime'):
         get_time_now = datetime.now().strftime("%H:%M")
 
-        start_time = datetime.strptime(get_config('System', 'startTime'), '%H:%M')
+        start_time = datetime.strptime(
+            get_config('System', 'startTime'), '%H:%M')
         start_time = start_time.strftime('%H:%M')
 
         end_time = datetime.strptime(get_config('System', 'endTime'), '%H:%M')
