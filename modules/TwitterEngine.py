264e3ae4343caf5784ae6105ec3cc8c7849559f8
Rached Mejri <r.mejri74100@gmail.com>
2020-06-27 01:29:15 +0200

    Adding OOP logic, refactor and rewrite the name of the fonction get_last_tweet

--- modules/TwitterEngine.py
+++ modules/TwitterEngine.py
@@ -20,20 +20,19 @@ access_token = get_config('TwitterAPI', 'access_token')
 access_token_secret = get_config('TwitterAPI', 'access_token_secret')
 
 
-def twitter_auth():
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+class TwitterEngine:
+    def __init__(self):
+        self.auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        self.auth.set_access_token(access_token, access_token_secret)
+        self.api = tweepy.API(self.auth)
 
-    api = tweepy.API(auth)
-    return api, auth
+    def is_there_a_last_tweet(self):
+        last_tweet = self.api.user_timeline(
+            id=get_config('TwitterAPI', 'user_id'), count=1)[0]
+        last_tweet_date = datetime.datetime.strptime(
+            str(last_tweet.created_at), '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d')
 
+        if todayDate == last_tweet_date and last_tweet.source == get_config('TwitterAPI', 'app_name'):
+            return True  # Déjà tweeté
 
-def get_last_tweet():
-    api, auth = twitter_auth()
-    last_tweet = api.user_timeline(id=get_config('TwitterAPI', 'user_id'), count=1)[0]
-    last_tweet_date = datetime.datetime.strptime(str(last_tweet.created_at), '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d')
-
-    if todayDate == last_tweet_date and last_tweet.source == get_config('TwitterAPI', 'app_name'):
-        return 1  # Déjà tweeté
-    else:
-        return 0  # On va chercher les données
+        return False  # On va chercher les données
